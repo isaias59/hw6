0ad5253f7ea2bcb0bae891f8b6caf24eba813220
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-02-21 15:39:19 -0800

    Delete gtest/src directory

--- gtest/src/square.cpp
+++
@@ -1,8 +0,0 @@
-#include "square.hpp"
-
-Square::Square(Point center, std::string name, int side)
-    : Rectangle(center, name, side, side) {}
-
-Square* Square::clone() const {
-    return new Square(*this);
-}
