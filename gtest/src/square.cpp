f34d5150e6ed61af74f74fc1a2fe93256c7d8258
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-02-21 15:12:19 -0800

    Create square.cpp

---
+++ gtest/src/square.cpp
@@ -0,0 +1,8 @@
+#include "square.h"
+
+Square::Square(Point center, std::string name, int side)
+    : Rectangle(center, name, side, side) {}
+
+Square* Square::clone() const {
+    return new Square(*this);
+}
